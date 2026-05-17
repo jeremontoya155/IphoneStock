b017e253050378319bbe7c74a4120a5931062555
Jeremontoya155 <jeremontoya155@gmail.com>
2026-05-17 20:36:04 -0300

    fix cambio prueba

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const compression = require('compression');
-
+// COMMIT
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.set('trust proxy', true);
